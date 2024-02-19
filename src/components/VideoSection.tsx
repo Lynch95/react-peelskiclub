f5bd09d86dd7da81f374760f76d0b8d32d3a2f8d
Lynch95 <barrylynch06@hotmail.com>
2024-02-19 14:23:16 +0000

    v1

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { HStack, Heading, AspectRatio, Box } from "@chakra-ui/react";
 
 const VideoSection = () => {
+
+  const videoIds = ['Kf53tBbA00A', 'dT9Xip4xRQ0',];
+
   return (
-    <div>VideoSection</div>
+    <>
+      <HStack justify='center' my={6}>
+        <Heading size={{base: 'x', lg: 'xl'}}>Videos</Heading>
+      </HStack>
+      <HStack justify="center" spacing={4} wrap="wrap">
+        {videoIds.map((videoId) => (
+          <Box key={videoId} p={2} w={{ base: '100%', md: '48%', lg: '31.33%' }}>
+            <AspectRatio ratio={16 / 9}>
+              <iframe
+                title="YouTube video"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                allowFullScreen
+              />
+            </AspectRatio>
+          </Box>
+        ))}
+      </HStack>
+    </>
   )
 }
 
-export default VideoSection
+export default VideoSection;
--- src/pages/Action.tsx
+++ src/pages/Action.tsx
@@ -1,11 +1,30 @@
-import Slideshow from '../components/Slideshow';
-
+import React from "react";
+import { Box, Divider, VStack } from "@chakra-ui/react";
+import Slideshow from "../components/Slideshow";
+import VideoSection from "../components/VideoSection";
 
 const Action = () => {
-  
-  return (
-    <Slideshow />
-  )
 
-}
+  const images = Array.from(
+    { length: 50 },
+    (_, i) => `/src/peelimages/${i + 1}.jpg`
+  );
+
+  const images2010 = Array.from(
+    { length: 176 }, 
+    (_, i) => `/src/peelimages/2010/${i + 1}.jpg`
+  );
+
+  return (
+    <VStack spacing={{ base: 4, lg: 8}} align="stretch">
+      <Slideshow images={images} heading="Slideshow - 2023 Trip" />
+     
+      <Divider my={{ base: 4, lg: 8}} borderColor="black" />
+      <Slideshow images={images2010} heading="2010 Trip" />
+      <Divider my={{ base: 4, lg: 8}} borderColor="black" />
+      <VideoSection />
+    </VStack>
+  );
+};
+
 export default Action;
